47d649205b0411a5c1919c3e950c94684e06852f
Heloi Neto <hey@heloineto.com>
2022-10-05 13:38:11 -0300

    feat: added SettingsPage

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foresight/shared/main_scaffold/main_scaffold.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:tailwind_colors/tailwind_colors.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -7,10 +9,39 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MainScaffold(
-      body: Center(
-        child: TextButton(
-          child: Text("Log out"),
-          onPressed: () {},
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: SizedBox(
+            height: 52,
+            width: double.infinity,
+            child: TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    return Colors.white;
+                  },
+                ),
+                backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    return TW3Colors.red.shade400;
+                  },
+                ),
+              ),
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(PhosphorIcons.signOut),
+                  SizedBox(width: 10),
+                  Text(
+                    'Sair',
+                    style: TextStyle(fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
