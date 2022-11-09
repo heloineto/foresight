f2720e74e95ee49a5cf091a05243d9383471c103
Heloi Neto <hey@heloineto.com>
2022-11-09 12:27:24 -0300

    feat: removed dividers

--- lib/investments/investment_page.dart
+++ lib/investments/investment_page.dart
@@ -17,171 +17,143 @@ class InvestmentPage extends StatelessWidget {
   final String indexer;
   final double rentabilityRate;
 
-  const InvestmentPage(
-      {super.key,
-      required this.bankName,
-      required this.investmentDate,
-      required this.investmentValue,
-      required this.product,
-      required this.indexer,
-      required this.rentabilityRate});
+  const InvestmentPage({
+    super.key,
+    required this.bankName,
+    required this.investmentDate,
+    required this.investmentValue,
+    required this.product,
+    required this.indexer,
+    required this.rentabilityRate,
+  });
 
   @override
   Widget build(BuildContext context) {
     return MainScaffold(
-        body: Padding(
-      padding: const EdgeInsets.only(top: 16, left: 25, right: 25),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 35.0),
-            child: Text(
-              'Investimento',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: TW3Colors.slate.shade700,
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16, left: 25, right: 25),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 35.0),
+              child: Text(
+                'Investimento',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: TW3Colors.slate.shade700,
+                ),
               ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15, bottom: 40),
-            child: Text(
-              brlFormatter.format(investmentValue),
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.normal,
-                color: TW3Colors.slate.shade700
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 40),
+              child: Text(
+                brlFormatter.format(investmentValue),
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.normal,
+                    color: TW3Colors.slate.shade700),
               ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  bankName,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    bankName,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
                   ),
-                ),
-                Text(
-                  dateTimeToString(investmentDate),
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
-                  ),
-                )
-              ],
+                  Text(
+                    dateTimeToString(investmentDate),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Rentabilidade',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rentabilidade',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
                   ),
-                ),
-                Text(
-                  '$rentabilityRate%',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
-                  ),
-                )
-              ],
+                  Text(
+                    '$rentabilityRate%',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Produto',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Produto',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
                   ),
-                ),
-                Text(
-                  product,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
-                  ),
-                )
-              ],
+                  Text(
+                    product,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Indexador',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Indexador',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
                   ),
-                ),
-                Text(
-                  indexer,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: TW3Colors.slate.shade800,
-                  ),
-                )
-              ],
+                  Text(
+                    indexer,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: TW3Colors.slate.shade800,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          // Divider(
-          //   color: TW3Colors.slate.shade500,
-          //   thickness: 1,
-          //   height: 1,
-          // ),
-        ],
+          ],
+        ),
       ),
     ));
   }
