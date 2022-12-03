d0d7500a97e0901b1919242b2bbd78db9e312251
Heloi Neto <hey@heloineto.com>
2022-12-03 20:51:16 -0300

    fix: button text

--- lib/investments/investments_form/investments_form.dart
+++ lib/investments/investments_form/investments_form.dart
@@ -175,7 +175,9 @@ class _InvestmentsFormState extends State<InvestmentsForm> {
                     ),
                     onPressed: onSubmit,
                     child: Text(
-                      'Criar Investimento',
+                      currentInvestment == null
+                          ? 'Criar Investimento'
+                          : 'Editar Investimento',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
