a0e7b6d2e240224a56e8ba441430408538ba9b82
Heloi Neto <hey@heloineto.com>
2022-12-06 13:32:38 -0300

    feat: add biometrics

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foresight/biometrics/biometrics.dart';
 import 'package:foresight/home/home.dart';
 import 'package:foresight/investment/investment.dart';
 import 'package:foresight/investments/investments_form/investments_form.dart';
@@ -6,8 +7,8 @@ import 'package:foresight/investments/investments_page.dart';
 import 'package:foresight/services/models.dart';
 import 'package:foresight/settings/settings.dart';
 
-Map<String, StatelessWidget Function(dynamic)> routes = {
-  '/': (context) => const HomePage(),
+Map<String, Widget Function(BuildContext)> routes = {
+  '/': (context) => const BiometricsPage(),
   '/investments': (context) => const InvestmentsPage(),
   '/investments/create': (context) => const InvestmentsFormPage(),
   '/investment': (context) {
